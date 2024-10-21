67d46d90de4f57b4386a3f6045912a4e92cb42a7
Archit <archittyagi599@gmail.com>
2024-10-21 06:29:33 +0530

    Exploring Access Modifiers

---
+++ OOPs/Company.java
@@ -0,0 +1,34 @@
+
+
+class AccessModifiers {
+    // main method
+    public static void main(String[] args){
+
+        // declaring private variables
+        int private_var = 10;
+
+        System.out.println("Code work successfully.");
+
+    
+    } // end of main method //
+
+} // end of main class //
+
+
+private class School {
+    // some varibles
+    String name = "DPM Sr. Sec. Public School";
+
+    void display_school_name(){
+        System.out.println(name);
+    }
+}
+
+class Company {
+    // some variables
+    String name = "Saddi Empire Pvt Ltd";
+
+    void display_company_name(){
+        System.out.println(name);
+    }
+}
